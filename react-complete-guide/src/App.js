cb29f091e52d15f899d5a89d917b5f20a5f16d82
vinibenso <vinibenso@gmail.com>
2022-10-28 15:40:43 -0300

    format date

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,50 +8,45 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12).toDateString() },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28).toDateString(),
+      date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 5, 12).toDateString(),
+      date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div>
-
+      <h2>Let's get started!</h2>
       <ExpenseItem
         title={expenses[0].title}
-        amout={expenses[0].amount}
+        amount={expenses[0].amount}
         date={expenses[0].date}
-      />
+      ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
-        amout={expenses[1].amount}
+        amount={expenses[1].amount}
         date={expenses[1].date}
-      />
+      ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
-        amout={expenses[2].amount}
+        amount={expenses[2].amount}
         date={expenses[2].date}
-      />
+      ></ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
-        amout={expenses[3].amount}
+        amount={expenses[3].amount}
         date={expenses[3].date}
-      />
-
-
-
-
-
+      ></ExpenseItem>
     </div>
   );
 }
-
 export default App;
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,19 +1,24 @@
 import './ExpenseItem.css'
 
-export function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28).toDateString()
-  const expenseTitle = 'Car Insurance'
-  const expenseAmount = 3800
+export function ExpenseItem(props) {
+  const day = props.date.toLocaleString('pt-BR', { day: '2-digit' })
+  const month = props.date.toLocaleString('pt-BR', { month: 'long' })
+  const year = props.date.getFullYear()
+
 
   return (
     <div className="expense-item">
       <div>
-        {expenseDate}
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+
       </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">
-          R$ {expenseAmount}
+          R${props.amount}
+
         </div>
       </div>
     </div>
